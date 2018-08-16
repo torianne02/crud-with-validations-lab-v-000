16536c5b4fabf365e9facd27aa127cf611737704
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-16 22:41:52 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :songs, only: [:show, :new, :edit, :create, :update]
-  delete '/songs/:id', to: 'songs#destroy, as:'song'
+  delete '/songs/:id', to: 'songs#destroy'
 end
